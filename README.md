194bae346d76141397df0f3d49fa73d3c1b5cef9
fora-soma <soma@for-a.co.jp>
2021-10-25 10:06:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# WebRTC Broadcasting Sample
+
+このプロジェクトは、WebRTC とブラウザを使って配信するサンプルプロジェクトです。
+
+がねこまさし氏の連載記事[WebRTC でキャスしよう！片方向リアルタイム映像配信を作ろう]に記載のソースコードを 2021/10 現在の API 仕様で動くように改変しています。
+
+引用元：https://html5experts.jp/mganeko/11444/
+
+# Requirement
+
+WebSocket サーバー/クライアント間は socket.io ライブラリを使用しています。
+
+- express 4.17.1
+- socket.io 4.3.1
+- socket.io-client 4.3.2
+
+# Installation
+
+上記ライブラリを npm で個別にインストールするか、
+`npm install` で依存ライブラリを一括インストールします。
+
+```bash
+npm install express
+npm install socket.io socket.io-client
+
+or
+
+npm install
+```
+
+# Usage
+
+git でリポジトリのクローンを作成後、`node server.js` でサーバーを実行。
+
+```bash
+git clone hhttps://github.com/fora-soma/webrtc_broadcast_sample.git
+cd webrtc_broadcast_sample
+npm server.js
+```
+
+ブラウザで下記ページを開き、送信側ページ（tx.html）で Start Video ボタンをクリックすると受信側ページで Web カメラの映像が表示されます。
+
+- http://localhost:9001/tx.html
+- http://localhost:9001/rx.html
+
+# Note
+
+現在、動作が不安定で Start Video ボタンをクリックしても受信側で映像が表示されないことが多いです。
+
+原因については現在調査中で、判明し次第修正いたします。
+
+# License
+
+"WebRTC Broadcasting Sample"は、社内での使用を目的としています。
+
+"WebRTC Broadcasting Sample" is intended for internal use.
---
+++ server.js
@@ -0,0 +1,98 @@
+/**
+ * シグナリングサーバー（WebSocketサーバー） + Webサーバー
+ */
+const BROADCAST_ID = "_broadcast_";
+
+// Express
+const express = require("express");
+const app = express();
+
+// publicディレクトリを公開
+app.use(express.static(__dirname + "/public"));
+
+const http = require("http");
+const server = http.createServer(app);
+
+// WebSocketサーバーにはsocket.ioを採用
+const io = require("socket.io")(server);
+
+// -- create the socket server on the port ---
+var port = 9001;
+server.listen(port);
+console.log("signaling server started on port:" + port);
+
+// This callback function is called every time a socket
+// tries to connect to the server
+io.on("connection", (socket) => {
+  // ---- multi room ----
+  socket.on("enter", function (roomName) {
+    socket.join(roomName);
+    console.log("id=" + socket.id + " enter room=" + roomName);
+    setRoomName(roomName);
+  });
+
+  function setRoomName(room) {
+    //// for v0.9
+    //socket.set('roomName', room);
+
+    // for v1.0
+    socket.roomName = room;
+  }
+
+  function getRoomName() {
+    var room = null;
+
+    //// for v0.9
+    //socket.get('roomName', function(err, _room) {
+    //  room = _room;
+    //});
+
+    // for v1.0
+    room = socket.roomName;
+
+    return room;
+  }
+
+  function emitMessage(type, message) {
+    // ----- multi room ----
+    var roomName = getRoomName();
+
+    if (roomName) {
+      console.log("===== message broadcast to room -->" + roomName);
+      socket.broadcast.to(roomName).emit(type, message);
+    } else {
+      console.log("===== message broadcast all");
+      socket.broadcast.emit(type, message);
+    }
+  }
+
+  // When a user send a SDP message
+  // broadcast to all users in the room
+  socket.on("message", function (message) {
+    message.from = socket.id;
+
+    // get send target
+    var target = message.sendto;
+    if (target && target != BROADCAST_ID) {
+      console.log("===== message emit to -->" + target);
+      socket.to(target).emit("message", message);
+      return;
+    }
+    // broadcast in room
+    emitMessage("message", message);
+  });
+
+  // When the user hangs up
+  // broadcast bye signal to all users in the room
+  socket.on("disconnect", function () {
+    console.log("-- user disconnect: " + socket.id);
+    // --- emit ----
+    emitMessage("user disconnected", { id: socket.id });
+
+    // --- leave room --
+    var roomName = getRoomName();
+    if (roomName) {
+      socket.leave(roomName);
+    }
+  });
+});
